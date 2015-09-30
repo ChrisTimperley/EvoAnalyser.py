bf7b3fc858b333fca852eba39c71674f8119ea7c
Chris Timperley <christimperley@googlemail.com>
2015-09-30 15:50:03 -0400

    More work on new log file reader.

--- src/reader.py
+++ src/reader.py
@@ -16,16 +16,49 @@ def split_log_into_sections(s):
 
 # Parses the contents of the meta section of a log file.
 def parse_meta(s):
-    return json.loads("{" + s + "}")
+    print "- parsing meta section."
+
+    # Remove the header and parse as a JSON object.
+    s = s[7:]
+    return json.loads(s)
 
 # Parses the contents of the environment section of a log file.
 def parse_environment(s):
-    return json.loads("{" + s + "}")
+    print "- parsing environment section."
+
+    # Remove the header and parse as a JSON object.
+    s = s[14:]
+    return json.loads(s)
 
 # Parses the contents of the problem section of a log file.
 def parse_problem(s):
-    return json.loads("{" + s + "}")
+    print "- parsing problem section."
+
+    # Remove the header and parse as a JSON object.
+    s = s[10:]
+    return json.loads(s)
 
 # Parses the contents of the search section of a log file.
 def parse_search(s):
-    pass
+    print "- parsing search section."
+    return s
+
+# Reads a given log file.
+def read_log(fn):
+    print "Opening log file: %s" % (fn)
+
+    # Open the log file and read each of its sections to separate strings.
+    with open(fn, "r") as f:
+        print "Opened log file: %s" % (fn)
+        meta, environment, problem, search = split_log_into_sections(f.read())
+        print "Split log file into sections."
+
+    # Parse each of the sections of the file.
+    meta = parse_meta(meta)
+    environment = parse_environment(environment)
+    problem = parse_problem(problem)
+    search = parse_search(search)
+
+    print "Finished parsing sections."
+
+read_log("test.log")
