f5e162f21836653360bf459947a3517a81465b16
Chris Timperley <christimperley@googlemail.com>
2015-09-30 22:19:05 -0400

    Added visualisation storage module.

---
+++ src/visualisation/storage.py
@@ -0,0 +1,14 @@
+# A dictionary is used to store registered visualisations.
+__visualisations = {}
+
+# Registers a provided visualisation using a given name.
+def register(name, cls):
+    if __visualisations.has_key(name):
+        raise Exception("Failed to register visualisation: key already defined (%s)" % (name))
+    __visualisations[name] = cls
+
+# Retrives a visualisation with a given name.
+def retrieve(name):
+    if __visualisations.has_key(name):
+        raise Exception("Failed to find visualisation: %s" % (name))
+    return __visualisations[name]
