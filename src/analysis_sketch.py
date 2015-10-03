a567e01180c311e0c43764a48517e0334bb38cf5
Chris Timperley <christimperley@googlemail.com>
2015-10-02 21:23:08 -0400

    Removed some old files.

--- src/analysis_sketch.py
+++
@@ -1,9 +0,0 @@
-class BestFitnessAnalysis(Analysis):
-
-    name = "best_fitness"
-
-    def compute(data):
-        fitnesses = filter(lambda p: p.fitness, data)
-
-        # Assume highest scalar value is best for now.
-        return max(fitnesses)
