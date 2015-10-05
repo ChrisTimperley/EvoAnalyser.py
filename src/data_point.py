e8d77bcca4d7ac334625771cc23d738d4f3ac26b
Chris Timperley <christimperley@googlemail.com>
2015-10-05 16:56:58 -0400

    Removed data point classes.

--- src/data_point.py
+++
@@ -1,9 +0,0 @@
-class DataPoint:
-
-    # Constructs a new data point.
-    def __init__(self):
-        pass
-
-    # Returns the unique identifier for this data point.
-    def uid(self):
-        return 0
