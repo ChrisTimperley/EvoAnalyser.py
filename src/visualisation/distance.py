a58e13fd5607b59b18ae41e1d12b92780da6b347
Chris Timperley <christimperley@googlemail.com>
2015-10-06 11:41:08 -0400

    Tidied up distance to origin visualisations.

--- src/visualisation/distance.py
+++ src/visualisation/distance.py
@@ -1,12 +1,22 @@
 import numpy as np
 
-def mean_distance_to_origin_vs_generation(data, options = {}):
+def __distance_to_origin_vs_generation(data, fun, options = {}):
     data = data.groupby('generation')
-    data = data.apply(lambda g: g.groupby('seed')['distance_to_origin'].aggregate(np.mean))
+    data = data.apply(lambda g: g.groupby('seed')['distance_to_origin'].aggregate(fun))
 
     # Plot the data.
     plot = data.plot(kind='line', legend=False)
-    plot.set_ylabel(options.get('y', 'Distance to original program (statements changed)'))
-    plot.set_xlabel(options.get('x', 'Generations Passed'))
-    plot.set_title(options.get('title', 'Distance to original program vs. generations passed'))
+    plot.set_ylabel(options['y'])
+    plot.set_xlabel(options.get('x', 'Generation'))
+    plot.set_title(options['title'])
     return plot
+
+"""
+Constructs a line graph showing the mean distance from each individual within
+the population to the origin at a given generation, for a collection of random
+seeds, for each problem.
+"""
+def mean_distance_to_origin_vs_generation(data, options = {}):
+    options['title'] = options.get('title', 'Mean distance to original program at each generation')
+    options['y'] = options.get('y', 'Mean distance to original program (statements changed)')
+    return __distance_to_origin_vs_generation(data, np.mean, options)
