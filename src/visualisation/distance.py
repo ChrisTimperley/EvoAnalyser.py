8a761c401840607371b03ad7a526c8f5015142db
Chris Timperley <christimperley@googlemail.com>
2015-10-06 12:06:58 -0400

    Added more distance visualisations.

--- src/visualisation/distance.py
+++ src/visualisation/distance.py
@@ -20,3 +20,33 @@ def mean_distance_to_origin_vs_generation(data, options = {}):
     options['title'] = options.get('title', 'Mean distance to original program at each generation')
     options['y'] = options.get('y', 'Mean distance to original program (statements changed)')
     return __distance_to_origin_vs_generation(data, np.mean, options)
+
+"""
+Constructs a line graph showing the median distance from each individual within
+the population to the origin at a given generation, for a collection of random
+seeds, for each problem.
+"""
+def median_distance_to_origin_vs_generation(data, options = {}):
+    options['title'] = options.get('title', 'Median distance to original program at each generation')
+    options['y'] = options.get('y', 'Median distance to original program (statements changed)')
+    return __distance_to_origin_vs_generation(data, np.median, options)
+
+"""
+Constructs a line graph showing the maximum distance from each individual within
+the population to the origin at a given generation, for a collection of random
+seeds, for each problem.
+"""
+def max_distance_to_origin_vs_generation(data, options = {}):
+    options['title'] = options.get('title', 'Max. distance to original program at each generation')
+    options['y'] = options.get('y', 'Max. distance to original program (statements changed)')
+    return __distance_to_origin_vs_generation(data, np.max, options)
+
+"""
+Constructs a line graph showing the minimum distance from each individual within
+the population to the origin at a given generation, for a collection of random
+seeds, for each problem.
+"""
+def min_distance_to_origin_vs_generation(data, options = {}):
+    options['title'] = options.get('title', 'Min. distance to original program at each generation')
+    options['y'] = options.get('y', 'Min. distance to original program (statements changed)')
+    return __distance_to_origin_vs_generation(data, np.min, options)
